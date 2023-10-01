7cdd36e3c3d32735bcc2eac404dd23692ea0324d
Mkien <manhkien1005@gmail.com>
2023-10-01 19:41:58 +0700

    Update Form1.cs

--- testGit/Form1.cs
+++ testGit/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,28 @@ namespace testGit
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void load_data()
+        {
+            string db = "Data Source=DESKTOP-F4JUFNO;Initial Catalog=ql_lichthi;Integrated Security=True";
+            SqlConnection con = new SqlConnection(db);
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Lich_coi_thi where Ca_Thi = '1' or Ca_Thi = '2' AND NgayThi ='2023-11-20'", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            adapter.Fill(dt);
+            grv1.DataSource = dt;
+            con.Close();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from Lich_coi_thi where Ca_Thi = '3' or Ca_Thi = '4' AND NgayThi ='2023-11-20'", con);
+            DataTable dt2 = new DataTable();
+            con.Open();
+            sqlDataAdapter.Fill(dt2);
+            grv2.DataSource = dt2;
+            con.Close();
+        }
+        private void btnDay1_Click(object sender, EventArgs e)
+        {
+            load_data();
         }
     }
 }
